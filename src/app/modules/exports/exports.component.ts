082dab15cb23f504f2b02ab59d804296757e3a71
Noecker, Patrick (DI FA TI COS TC 4) <patrick.noecker@siemens.com>
2021-10-15 14:44:12 +0200

    export works, tooltip changed

--- src/app/modules/exports/exports.component.ts
+++ src/app/modules/exports/exports.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import * as FileSaver from 'file-saver';
 import { DeviceInput } from 'src/shared/device-input';
-import { MainComponent } from '../main/main.component';
 
 @Component({
   selector: 'app-exports',
@@ -9,30 +8,23 @@ import { MainComponent } from '../main/main.component';
   styleUrls: ['./exports.component.scss']
 })
 export class ExportsComponent implements OnInit {
-  inputs: DeviceInput[] = [];
+
+  @Input() populatedInputs?: DeviceInput[];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  // Put in new component -> Make export work -> make (?) work -> -scss Doku lesen (difference zu .less) -> angular modal popup nachsehen
+  // angular populatedInputs.length anzeigen -> make (?) work -> -scss Doku lesen (difference zu .less) -> angular modal popup nachsehen
   exportAsJson(): void{
-    // Add export logic 
-
-    // Output [] empty array
-    console.log(this.inputs);
+    console.log(this.populatedInputs);
     
-    let data = JSON.stringify(this.inputs);
+    let data = JSON.stringify(this.populatedInputs, null, 2);
     console.log(data);
-
-    let test = "some test string"
-    console.log(test);
     
-    let blob = new Blob([test], {type: 'application/json'});
+    let blob = new Blob([data], {type: 'application/json'});
     console.log(blob);
-    
-    //let file = new File([blob], "InventoryList", {type: "application/json"});
 
     FileSaver.saveAs(blob, "InventoryList");
 
